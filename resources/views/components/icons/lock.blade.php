cc16274750088e0bce0e60bc7c573c6c8da046d2
praga arya <aryapragar@gmail.com>
2026-07-02 15:57:47 +0700

    menambahkan svg icon lock component

---
+++ resources/views/components/icons/lock.blade.php
@@ -0,0 +1,4 @@
+
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0V10.5m-2.25 10.5h13.5c.621 0 1.125-.504 1.125-1.125v-7.5c0-.621-.504-1.125-1.125-1.125H5.25c-.621 0-1.125.504-1.125 1.125v7.5c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
