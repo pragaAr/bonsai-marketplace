5e7d8fee77142a7453841e7e07446f10fa4c6043
praga arya <aryapragar@gmail.com>
2026-07-02 15:59:25 +0700

    ubah placeholder format penulisan name permission, ubah class untuk text error

--- resources/views/livewire/admin/access/permission.blade.php
+++ resources/views/livewire/admin/access/permission.blade.php
@@ -155,10 +155,10 @@
             Nama Permission
           </label>
           <input wire:model="name" type="text"
-            placeholder="contoh: access.roles.view"
+            placeholder="contoh: roles.manage"
             class="w-full px-4 py-2.5 rounded-xl border border-primary/20 text-sm text-primary focus:border-primary/40 outline-none">
           @error('name')
-            <p class="mt-1 text-xs text-danger-600">
+            <p class="mt-1 text-xs text-red-600">
               {{ $message }}</p>
           @enderror
         </div>
@@ -172,7 +172,7 @@
             placeholder="contoh: Lihat Role"
             class="w-full px-4 py-2.5 rounded-xl border border-primary/20 text-sm text-primary focus:border-primary/40 outline-none">
           @error('label')
-            <p class="mt-1 text-xs text-danger-600">
+            <p class="mt-1 text-xs text-red-600">
               {{ $message }}</p>
           @enderror
         </div>
