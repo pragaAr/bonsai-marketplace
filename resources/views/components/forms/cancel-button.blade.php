5f9442854250cd1d80bb26bf9b645639e45d7f40
praga arya <aryapragar@gmail.com>
2026-07-03 15:35:05 +0700

    menambahkan styling background

--- resources/views/components/forms/cancel-button.blade.php
+++ resources/views/components/forms/cancel-button.blade.php
@@ -5,7 +5,7 @@
 <button type="{{ $type }}"
   {{ $attributes->merge([
       'class' =>
-          'flex-1 px-4 py-2.5 border border-primary/10 rounded-xl text-sm font-medium text-primary hover:bg-primary/5 cursor-pointer',
+          'flex-1 px-4 py-2.5 border border-primary/10 bg-primary/10 rounded-xl text-sm font-medium text-primary hover:bg-primary/5 cursor-pointer',
   ]) }}>
   {{ $slot }}
 </button>
