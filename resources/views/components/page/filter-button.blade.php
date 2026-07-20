2c0cce5ad7bb3438acc572aa40f0309ad9ce5fa2
praga arya <aryapragar@gmail.com>
2026-07-20 15:53:03 +0700

    ubah reusable component yang dipanggil

--- resources/views/components/page/filter-button.blade.php
+++ resources/views/components/page/filter-button.blade.php
@@ -7,7 +7,7 @@
   wire:target="{{ $target }}"
   {{ $attributes->merge(['class' => 'w-full sm:w-auto px-3 py-2 bg-black/50 text-white text-sm font-semibold rounded-xl hover:shadow-lg transition-smooth cursor-pointer inline-flex items-center justify-center gap-1 disabled:opacity-50 self-start sm:self-center']) }}>
 
-  <x-icons.arrow-up-down wire:loading.remove
+  <x-icons.arrow-down-up wire:loading.remove
     wire:target="{{ $target }}"
     class="h-3.5 w-3.5 text-current" />
 
