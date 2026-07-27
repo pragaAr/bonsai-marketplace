7178bb60d4aea5b42716b5d2532eaf0803d1fac8
praga arya <aryapragar@gmail.com>
2026-07-27 15:16:32 +0700

    menambah reusable component icon

--- resources/views/components/icons/arrow-right.blade.php
+++ resources/views/components/icons/arrow-right.blade.php
@@ -1,5 +1,5 @@
-<svg {{ $attributes }} fill="none" stroke="currentColor"
-  stroke-width="2" viewBox="0 0 24 24">
+<svg {{ $attributes }} fill="none" viewBox="0 0 24 24"
+  stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round"
-    d="M17 8l4 4m0 0l-4 4m4-4H3" />
+    d="M14 5l7 7m0 0l-7 7m7-7H3" />
 </svg>
