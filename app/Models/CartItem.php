9fd1d70f30093c003c531fb37cc9de9146ebd26b
praga arya <aryapragar@gmail.com>
2026-08-01 11:49:15 +0700

    cart dan cartitem model

---
+++ app/Models/CartItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CartItem extends Model
+{
+    protected $fillable = ['cart_id', 'product_id', 'qty'];
+
+    protected $casts = [
+        'cart_id' => 'integer',
+        'product_id' => 'integer',
+        'qty' => 'integer',
+    ];
+
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
